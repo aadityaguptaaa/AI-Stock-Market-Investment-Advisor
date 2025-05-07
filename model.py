cf655e12e078da00ec69867fa90123dedc968773
Aaditya <aadityavidit@gmail.com>
2025-05-07 23:30:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from keras.models import Sequential # type: ignore
 from keras.layers import Dense, LSTM # type: ignore
 from datetime import datetime, timedelta   
  
-# Initialize scaler
+# Initialize scaler 
 scaler = MinMaxScaler(feature_range=(0, 1)) 
   
 # Available stocks (NSE symbols)
