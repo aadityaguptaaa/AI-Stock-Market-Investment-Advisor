a45c8daf8d6ac29e6428e8cd627a96d6714da0ca
Aaditya <aadityavidit@gmail.com>
2025-05-08 23:45:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential # type: ignore
 from keras.layers import Dense, LSTM # type: ignore
-from datetime import datetime, timedelta   
- 
+
+
 # Initialize scaler 
 scaler = MinMaxScaler(feature_range=(0, 1)) 
   
